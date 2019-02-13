1d762e904911619ecee449441f4c475056a29669
moraispgsi <moraispgsi@gmail.com>
2019-02-13 21:37:14 +0000

    Added previously uncommited files.

---
+++ java-refactoring/src/test/resources/rules/recycle/test2/Before.java
@@ -0,0 +1,11 @@
+package recycle1;
+
+public class Recycle {
+    public void wrong1(AttributeSet attrs , int defStyle) {
+        final TypedArray a = getContext().obtainStyledAttributes(attrs, new int[]{0}, defStyle, 0);
+        String example = a.getString(0);
+        if(a != null) {
+            a.recycle();
+        }
+    }
+}
