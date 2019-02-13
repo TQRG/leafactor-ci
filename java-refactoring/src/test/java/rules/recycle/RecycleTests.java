44f33c4dc30d631f8ac4f1c28330ab17ebf5946b
moraispgsi <moraispgsi@gmail.com>
2019-02-13 21:35:47 +0000

    JUnit dynamic automated testing with TestFactory (for Recycle rule for now).

--- java-refactoring/src/test/java/rules/recycle/RecycleTests.java
+++ java-refactoring/src/test/java/rules/recycle/RecycleTests.java
@@ -1,26 +1,32 @@
 package rules.recycle;
 
-import com.github.difflib.DiffUtils;
-import com.github.difflib.algorithm.DiffException;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
-import org.junit.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import rules.RecycleRefactoringRule;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class RecycleTests {
+class RecycleTests {
     private static final PrintStream out = System.out;
     private static final PrintStream dummy = new PrintStream(new OutputStream() {@Override public void write(int b){} });
+
     private static void togglePrints(boolean on) {
         System.setOut(on?out:dummy);
+    }
+
+    private String relativePathToAbsolutePath(String relativePath) {
+        return RecycleTests.class.getResource(relativePath).getPath().substring(1);
     }
 
     private String loadSample(String relativePath) throws IOException {
@@ -28,21 +34,39 @@ public class RecycleTests {
         return new String(Files.readAllBytes(Paths.get(absolutePath)));
     }
 
-    @Test
-    public void testBasic() throws IOException, DiffException {
-        togglePrints(false);
-        // Load input files
-        String beforeSample = loadSample("./BasicBefore.java");
-        String afterSample = loadSample("./BasicAfter.java");
+    @TestFactory
+    Collection<DynamicTest> dynamicTestsWithCollection() throws IOException {
+        String dir = relativePathToAbsolutePath("./");
+        try (Stream<Path> paths = Files.walk(Paths.get(dir))) {
+            return paths
+                    .filter((file) ->  !file.equals(Paths.get(dir)))
+                    .filter(Files::isDirectory)
+                    .map((file) -> DynamicTest.dynamicTest(file.getFileName().toString(),
+                            () -> {
+                                togglePrints(true);
 
-        // Apply refactoring
-        CompilationUnit beforeCompilationUnit = LexicalPreservingPrinter.setup(JavaParser.parse(beforeSample));
-        RecycleRefactoringRule rule = new RecycleRefactoringRule();
-        rule.apply(beforeCompilationUnit);
-        String after = LexicalPreservingPrinter.print(beforeCompilationUnit);
+                                String beforePath = file.toAbsolutePath() + "\\Before.java";
+                                String afterPath = file.toAbsolutePath() + "\\After.java";
 
-        // Compare result with the sample
-        assert(after.equals(afterSample));
+                                // Load input files
+                                System.out.println("[" + file.getFileName().toString() + "] Loading Files");
+                                String beforeSample = new String(Files.readAllBytes(Paths.get(beforePath)));
+                                String afterSample = new String(Files.readAllBytes(Paths.get(afterPath)));
+
+                                // Apply refactoring
+                                System.out.println("[" + file.getFileName().toString() + "] Finding and refactoring opportunities");
+                                togglePrints(false);
+                                CompilationUnit beforeCompilationUnit = LexicalPreservingPrinter.setup(JavaParser.parse(beforeSample));
+                                RecycleRefactoringRule rule = new RecycleRefactoringRule();
+                                rule.apply(beforeCompilationUnit);
+                                String after = LexicalPreservingPrinter.print(beforeCompilationUnit);
+
+                                togglePrints(true);
+                                System.out.println("[" + file.getFileName().toString() + "] Comparing result");
+                                // Compare result with the sample
+                                assert(after.equals(afterSample));
+                            })).collect(Collectors.toList());
+        }
     }
 
 }
